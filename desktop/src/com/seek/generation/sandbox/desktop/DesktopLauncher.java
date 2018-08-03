945d7fd09a697b258e562eaac8c6d035debf1780
Seek Generation <seekgeneration@gmail.com>
2018-08-03 13:44:54 +0100

    initial setup

---
+++ desktop/src/com/seek/generation/sandbox/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.seek.generation.sandbox.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.seek.generation.sandbox.Sandbox;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Sandbox(), config);
+	}
+}
