e1a607e829b5636f1ab88036ae83e11ae8cf0144
Seek Generation <seekgeneration@gmail.com>
2018-08-18 21:05:19 +0100

    Changed World type to allow for soft bodies

--- core/src/com/seek/generation/sandbox/physics/PhysicsWorld.java
+++ core/src/com/seek/generation/sandbox/physics/PhysicsWorld.java
@@ -21,10 +21,12 @@ import com.badlogic.gdx.physics.bullet.dynamics.btDynamicsWorld;
 import com.badlogic.gdx.physics.bullet.dynamics.btRigidBody;
 import com.badlogic.gdx.physics.bullet.dynamics.btSequentialImpulseConstraintSolver;
 import com.badlogic.gdx.physics.bullet.linearmath.btIDebugDraw;
+import com.badlogic.gdx.physics.bullet.softbody.btSoftBody;
+import com.badlogic.gdx.physics.bullet.softbody.btSoftRigidDynamicsWorld;
 
 public class PhysicsWorld {
 
-    private final btDynamicsWorld dynamicsWorld;
+    private final btSoftRigidDynamicsWorld dynamicsWorld;
     private final btCollisionConfiguration collisionConfiguration;
     private final btConstraintSolver constrainSolver;
     private final btBroadphaseInterface broadphase;
@@ -38,14 +40,20 @@ public class PhysicsWorld {
     public Vector3 rayFrom = new Vector3();
     public Vector3 rayTo = new Vector3();
 
-    public PhysicsWorld(){
+    private final short DEFAULT_FLAG = -1;
+    private final short DEFAULT_GROUP = DEFAULT_FLAG;
+    private final short DEFAULT_MASK = DEFAULT_FLAG;
+
+    public PhysicsWorld() {
         Bullet.init();
 
         collisionConfiguration = new btDefaultCollisionConfiguration();
         constrainSolver = new btSequentialImpulseConstraintSolver();
         broadphase = new btDbvtBroadphase();
         dispatcher = new btCollisionDispatcher(collisionConfiguration);
-        dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, constrainSolver, collisionConfiguration);
+//        dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, constrainSolver, collisionConfiguration);
+
+        dynamicsWorld = new btSoftRigidDynamicsWorld(dispatcher, broadphase, constrainSolver, collisionConfiguration);
 
         debugDrawer = new DebugDrawer();
         debugDrawer.setDebugMode(btIDebugDraw.DebugDrawModes.DBG_MAX_DEBUG_DRAW_MODE);
@@ -54,7 +62,7 @@ public class PhysicsWorld {
         rayResultCallback = new ClosestRayResultCallback(Vector3.Zero, Vector3.Z);
     }
 
-    public btCollisionObject rayTest(Camera camera){
+    public btCollisionObject rayTest(Camera camera) {
         Ray ray = camera.getPickRay(Gdx.input.getX(), Gdx.input.getY());
         rayFrom.set(ray.origin);
         rayTo.set(ray.direction).scl(50f).add(rayFrom);
@@ -66,7 +74,7 @@ public class PhysicsWorld {
 
         dynamicsWorld.rayTest(rayFrom, rayTo, rayResultCallback);
 
-        if(rayResultCallback.hasHit()){
+        if (rayResultCallback.hasHit()) {
             final btCollisionObject obj = rayResultCallback.getCollisionObject();
 
             return obj;
@@ -75,31 +83,38 @@ public class PhysicsWorld {
         return null;
     }
 
-    public void debugDraw(Camera camera){
+    public void debugDraw(Camera camera) {
         debugDrawer.begin(camera);
         dynamicsWorld.debugDrawWorld();
         debugDrawer.end();
     }
 
-    public void step(float timeStep, int maxSubSteps, float fixedTimeStep){
+    public void step(float timeStep, int maxSubSteps, float fixedTimeStep) {
         dynamicsWorld.stepSimulation(timeStep, maxSubSteps, fixedTimeStep);
     }
 
-    public void step()
-    {
-        final float timeStep = Math.min(1f/30f, Gdx.graphics.getDeltaTime());
-        step(timeStep, 5, 1f/60f);
+    public void step() {
+        final float timeStep = Math.min(1f / 30f, Gdx.graphics.getDeltaTime());
+        step(timeStep, 5, 1f / 60f);
     }
 
-    public void addRigidBody(btRigidBody rigidBody){
-        dynamicsWorld.addRigidBody(rigidBody);
+    public void addSoftBody(btSoftBody softBody){
+        dynamicsWorld.addSoftBody(softBody, DEFAULT_GROUP, DEFAULT_MASK);
     }
 
-    public void addRigidBody(btRigidBody rigidBody, short group, short mask){
+    public void addSoftBody(btSoftBody softBody, short group, short mask){
+        dynamicsWorld.addSoftBody(softBody, group, mask);
+    }
+
+    public void addRigidBody(btRigidBody rigidBody) {
+        dynamicsWorld.addRigidBody(rigidBody, DEFAULT_GROUP, DEFAULT_MASK);
+    }
+
+    public void addRigidBody(btRigidBody rigidBody, short group, short mask) {
         dynamicsWorld.addRigidBody(rigidBody, group, mask);
     }
 
-    public void addCollisionObject(btCollisionObject collisionObject){
+    public void addCollisionObject(btCollisionObject collisionObject) {
         dynamicsWorld.addCollisionObject(collisionObject);
     }
 
@@ -107,17 +122,16 @@ public class PhysicsWorld {
         dynamicsWorld.addCollisionObject(collisionObject, groups, mask);
     }
 
-    public void addAction(btActionInterface action){
+    public void addAction(btActionInterface action) {
         dynamicsWorld.addAction(action);
     }
 
-    public void setGravity(float x, float y, float z){
+    public void setGravity(float x, float y, float z) {
         tmp.set(x, y, z);
         dynamicsWorld.setGravity(tmp);
     }
 
-    public void dispose()
-    {
+    public void dispose() {
         dynamicsWorld.dispose();
         collisionConfiguration.dispose();
         constrainSolver.dispose();
